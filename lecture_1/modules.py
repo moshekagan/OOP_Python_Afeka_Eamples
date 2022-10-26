32655306fef3c5c989f89114977e1fa1050c5ca5
moshekagan <shaikagan!1>
2022-10-26 17:25:21 +0300

    modules

---
+++ lecture_1/modules.py
@@ -0,0 +1,24 @@
+# Example 1 - how to import a module:
+# We will import module random
+import random
+
+# using function from random module
+
+# random - return a random a fload number between 0.0 to 1.0
+some_random_flot_number = random.random()
+
+# randint - return a random integer number between the giving range
+some_random_integer_number = random.randint(3, 9)
+
+# ----------------------------------------------------------------------------------------------------
+
+# Example 2 - how to import a module:
+# We will import math module
+import math
+
+# Example 2 - using function from math module
+
+# sqrt - Returns the square root of a number
+res1 = math.sqrt(4)
+print(res1)     # 2
+
