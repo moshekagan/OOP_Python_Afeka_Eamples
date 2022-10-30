a623a60e53e972ef12834f309d0404c71aa2a580
moshekagan <shaikagan!1>
2022-10-30 19:52:03 +0200

    write into csv file

--- lecture_1/csv_example.py
+++ lecture_1/csv_example.py
@@ -1,5 +1,8 @@
 # import csv module
 import csv
+# ------------------------
+# Read from CSV file
+# ------------------------
 
 # open the file 'example_csv_file.csv' (this is the file path) and save it in the variable: csv_file
 with open("example_csv_file.csv") as csv_file:
@@ -19,3 +22,26 @@ with open("example_csv_file.csv") as csv_file:
 
         # printing the name and his grade
         print(f"Name is {name} and the grade is {grade}")
+
+
+# ------------------------
+# Write into CSV file
+# ------------------------
+
+# open the file 'example_csv_file.csv' in mode "a" (Append) - will append to the end of the file
+# Also possible to open in "w" (Write) mode - will overwrite any existing content
+with open("example_csv_file.csv", mode='a') as csvfile:
+    # create CSV writer from the file
+    writer = csv.writer(csvfile)
+
+    # read input from the user to insert as a new line to the file
+    name = input("insert a student name: ")
+    grade = int(input("insert a student name: "))
+
+    # creating a list as a row of values
+    row = [name, grade]
+    # writing to the end of the file the new row
+    writer.writerow(row)
+
+    # Don't forget to close the file after writing!!!
+    csvfile.close()
