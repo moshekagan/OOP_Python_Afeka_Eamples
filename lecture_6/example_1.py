2a2fbec972b57960982bab020b96c1efff7620d4
moshekagan <shaikagan!1>
2022-12-08 18:18:56 +0200

    fruits 2

--- lecture_6/example_1.py
+++ lecture_6/example_1.py
@@ -1,7 +1,11 @@
 file_path = "temp.csv"
+
 try:
     with open(file_path) as file:
         file.read()
+    print("Success")
 except FileNotFoundError as e:
     print(e)
     print(f"Couldn't open the file: {file_path}")
+
+print("Done")
